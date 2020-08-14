503cf8168f9676a187523f47594ae39fac7578aa
No000 <sk.777.main.9@gmail.com>
2020-08-15 00:21:11 +0900

    CreateFinalDevPath

---
+++ No9/CreateFinalDevPath/main.c
@@ -0,0 +1,33 @@
+#include "efi.h"
+#include "common.h"
+
+void efi_main(void *ImageHandle __attribute__ ((unused)),
+         struct EFI_SYSTEM_TABLE *SystemTable)
+{
+    struct EFI_LOADED_IMAGE_PROTOCOL *lip;
+    struct EFI_DEVICE_PATH_PROTOCOL *dev_path;
+    unsigned long long status;
+
+    efi_init(SystemTable);
+    cls();
+
+    // ImageHandleのEFI_LOADIMAGE_PROTOCOL(lip)を取得
+    // デバイスパスを調べてくれて取得する
+    status = ST->BootServices->OpenProtocol(
+        ImageHandle, &lip_guid, (void **)&lip, ImageHandle, NULL,
+        EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    assert(status, L"OpenProtocol(lip)");
+
+    // lip->DeviceHandleのEFI_DEVICE_PATH_PROTOCOL(dev_path)を取得
+    status = ST->BootServices->OpenProtocol(
+        lip->DeviceHandle, &dpp_guid, (void **)&dev_path, ImageHandle,
+        NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
+    assert(status, L"OpenProtocol(dpp)");
+
+    // dev_pathをテキストに変換し表示(これでデバイス（HDDやUSBのROOTを指定できるようになった！）)
+    puts(L"dev_path: ");
+    puts(DPTTP->ConvertDevicePathToText(dev_path, FALSE, FALSE));
+    puts(L"\r\n");
+
+    while (TRUE);
+}
